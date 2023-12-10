ae85e425924cfbbcca372468ea5bc06667bbec98
Michael Terry <mterry@sancsoft.com>
2023-12-09 20:51:01 -0500

    initial version for platform testing

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO.Ports;
+
+Console.WriteLine("Serial Ports:");
+string[] ports = SerialPort.GetPortNames();
+
+foreach (string port in ports)
+{
+    Console.Write($"{port}: ");
+    try
+    {
+        SerialPort sp = new SerialPort(port, 9600);
+        sp.Open();
+        sp.Close();
+        Console.WriteLine("Available");
+    }
+    catch (UnauthorizedAccessException)
+    {
+        Console.WriteLine("In Use");
+    }
+    catch
+    {
+        Console.WriteLine("Error");
+    }
+}
+
+if (ports.Length == 0)
+{
+    Console.WriteLine("None");
+}
+
